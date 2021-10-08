3f2892be5dc4d2114e34bb9e399bff4b62ec5b9f
[LL] Benjamin CORDREAUX <cordreaux.benjamin@gmail.com>
2021-10-08 10:52:46 +0200

    Finalisation démarche

--- script.js
+++ script.js
@@ -16,51 +16,23 @@ const submitForm = document.querySelector(".formulaire");
 // });
 
 //Script Démarches et Services//
-const adminIcon = document.querySelector(".grid__icon", ".grid__icon1");
-const adminText = document.querySelector("#admin");
-const transportIcon = document.querySelector(".grid__icon grid__icon2");
-const transportText = document.querySelector("#transport");
-const healthIcon = document.querySelector(".grid__icon grid__icon3");
-const healthText = document.querySelector("#health");
-const teachIcon = document.querySelector(".grid__icon grid__icon4");
-const teachText = document.querySelector("#teach");
-const socialIcon = document.querySelector(".grid__icon grid__icon5");
-const socialText = document.querySelector("#social");
-const publicMarketIcon = document.querySelector(".grid__icon grid__icon6");
-const publicMarketText = document.querySelector("#publicMarket");
 
-const menuSelector = document.querySelector("#formalitySelect");
-
-menuSelector.addEventListener("select", () => {
-
-  
-})
-
-adminIcon.addEventListener("click", () =>{
-  adminText.style.display="block";
 //pour ouvrir le modal en cliquant sur le boutton contact
-if (contactButton) {
-  contactButton.addEventListener("click", () => {
-    contactModal.style.display = "flex";
-  });
-}
-// pour ouvrir et fermer le modal via la croix
-if (closeModal) {
-  closeModal.addEventListener("click", () => {
-    contactModal.style.display = "none";
-  });
-}
 
-if (submitForm) {
-  submitForm.addEventListener("submit", (e) => {
-    e.preventDefault();
-    alert("Votre message a bien été envoyé !");
-  });
-}
-if (contactModal) {
-  contactModal.addEventListener("click", (e) => {
-    console.log(e.target);
-    console.log(e.currentTarget);
-    e.target.id == "myModal" ? (contactModal.style.display = "none") : null;
-  });
-}
+contactButton?.addEventListener("click", () => {
+  contactModal.style.display = "flex";
+});
+// pour ouvrir et fermer le modal via la croix
+closeModal?.addEventListener("click", () => {
+  contactModal.style.display = "none";
+});
+
+submitForm?.addEventListener("submit", (e) => {
+  e.preventDefault();
+  alert("Votre message a bien été envoyé !");
+});
+contactModal?.addEventListener("click", (e) => {
+  console.log(e.target);
+  console.log(e.currentTarget);
+  e.target.id == "myModal" ? (contactModal.style.display = "none") : null;
+});
